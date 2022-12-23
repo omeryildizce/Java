0ac5dd5fdce0469cd98b3f5dd10134aad855f5f5
omeryildizce <omeryildizce@gmail.com>
2022-12-23 19:39:25 +0300

    IOC ve Dependency Injection

---
+++ springDrmoIOCAnnotation/src/com/springdemo/Main.java
@@ -0,0 +1,13 @@
+package com.springdemo;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+public class Main { 
+	public static void main(String[] args) {
+		AnnotationConfigApplicationContext context = 
+				new AnnotationConfigApplicationContext(IocConfig.class);
+		
+		ICustomerService customerService = 
+				context.getBean("service", ICustomerService.class);
+		//context.refresh();
+		customerService.add();
+	} 
+}
